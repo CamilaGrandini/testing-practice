63587cd5ead79fdcd3e3a870a84983b102021887
Camila <camilabrito@ciandt.com>
2021-11-17 17:22:54 -0500

    adding integration tests for the basic operations (function math)

--- 02-integration/01-calc.js
+++ 02-integration/01-calc.js
@@ -1,8 +1,68 @@
 // ==================================================
 // DEFINE FUNCTION(S)
 // ==================================================
-function calc() {
+function sum(num1, num2) {
 
+  if (num1 == null && num2 == null) {
+    return 0;
+  } else if (num2 == null) {
+    return num1;
+  }
+
+  return num1 + num2;
+}
+
+function difference(num1, num2) {
+  if (num1 == null && num2 == null) {
+    return 0;
+
+  } else if (num2 == null) {
+    return num1;
+  } else {
+    return num1 - num2;
+  }
+}
+
+function product(num1, num2) {
+  if (num1 == 0 || num2 == 0) {
+    return 0;
+  }
+
+  if (num1 == null && num2 == null) {
+    return 0;
+  } else if (num2 == null) {
+    return num1;
+  }
+
+  return num1 * num2;
+}
+
+function quotient(num1, num2) {
+  if (num2 == 0) {
+    return 'ERROR';
+  }
+
+  if (num1 == null && num2 == null) {
+    return 0;
+  } else if (num2 == null) {
+    return num1;
+  }
+  return num1 / num2;
+}
+
+function calc(operation, num1, num2) {
+  switch (operation) {
+    case "add":
+      return sum(num1, num2);
+    case "subtract":
+      return difference(num1, num2);
+    case "multiply":
+      return product(num1, num2);
+    case "divide":
+      return quotient(num1, num2);
+    case "exponent":
+      return console.log("Operation not supported");
+  }
 }
 
 // ==================================================
@@ -20,27 +80,39 @@ try {
   // Test Case 2
   // --------------------------------------------------
   // It should return the correct difference when the user provides: 'subtract', 20, 10.
+  var result = calc('subtract', 20, 10);
+  if (result !== 10) throw new Error('Expected calc("subtract", 20, 10) to be 10. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 3
   // --------------------------------------------------
   // It should return the correct product when the user provides: 'multiply', 9, 9.
+  var result = calc('multiply', 9, 9);
+  if (result !== 81) throw new Error('Expected calc("multiply", 9, 9) to be 81. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 4
   // --------------------------------------------------
   // It should return the correct quotient when the user provides: 'divide', 9, 3.
+  var result = calc('divide', 9, 3);
+  if (result !== 3) throw new Error('Expected calc("divide", 9, 3) to be 3. Received: ' + result);
 
   // --------------------------------------------------
   // Test Case 5
   // --------------------------------------------------
   // It should return the message 'Operation not supported.' when the user provides: exponent, 2, 8.
+  var result = calc("exponent", 2, 8);
+  if (result !== undefined)
+    throw new Error(
+      'Expected calc("exponent", 2, 8) to be Operation not supported. Received: ' +
+      result
+    );
 
   console.log('All tests passed successfully.');
 
-// ==================================================
-// PRINT ERRORS
-// ==================================================
+  // ==================================================
+  // PRINT ERRORS
+  // ==================================================
 } catch (e) {
   console.warn('Whoops, the following test did not pass:');
   console.error(e.message);
